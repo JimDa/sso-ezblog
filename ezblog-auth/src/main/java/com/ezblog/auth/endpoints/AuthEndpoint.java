bd395b759c1a1f1f4f3ffbaba6f76627865e0a92
dpc <dapengcheng@indexvc.com>
2019-11-06 11:16:28 +0800

    代码提交

--- ezblog-auth/src/main/java/com/ezblog/auth/endpoints/AuthEndpoint.java
+++ ezblog-auth/src/main/java/com/ezblog/auth/endpoints/AuthEndpoint.java
@@ -1,16 +1,18 @@
 package com.ezblog.auth.endpoints;
 
-import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AuthEndpoint {
-    @RequestMapping("/oauth/exit")
-    public void exit(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) {
-        new SecurityContextLogoutHandler().logout(httpServletRequest, null, null);
+
+    @GetMapping("/login")
+    public String login() {
+        return "page-login";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "page-signup";
     }
 }
